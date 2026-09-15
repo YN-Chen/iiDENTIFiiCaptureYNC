f2b5e3544626dbe6fc4f3df2fe24def2e3f07158
Yi-Nain Chen <illyctc007@gmail.com>
2026-09-15 10:52:03 +0200

    Initial commit: Base Xcode project

---
+++ iiDENTIFiiCaptureYNC/iiDENTIFiiCaptureYNC/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iiDENTIFiiCaptureYNC
+//
+//  Created by Yi-Nain Chen on 2026/09/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iiDENTIFiiCaptureYNC/iiDENTIFiiCaptureYNC/iiDENTIFiiCaptureYNCApp.swift
@@ -0,0 +1,17 @@
+//
+//  iiDENTIFiiCaptureYNCApp.swift
+//  iiDENTIFiiCaptureYNC
+//
+//  Created by Yi-Nain Chen on 2026/09/15.
+//
+
+import SwiftUI
+
+@main
+struct iiDENTIFiiCaptureYNCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
